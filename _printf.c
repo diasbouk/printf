61974314b5443517b0743e30b2f5183d99c6fc7e
diasbouk <boukdsaid@gmail.com>
2023-10-14 18:02:57 +0100

    task 0

--- _printf.c
+++ _printf.c
@@ -7,36 +7,37 @@
  */
 int _printf(const char *format, ...)
 {
-	int i = 0;
+	unsigned int i = 0;
+	unsigned int j = 0;
 	char *str;
 	va_list args;
 		va_start(args, format);
 		if (format == NULL)
 			return (-1);
-		while (format[i])
+		while (format[i] != '\0')
 		{
 			if (format[i] == '%')
 			{
 				i++;
 				if (format[i] == 'c')
 				{
-					write(1, va_arg(args, char *), 1);
+					i++;
+					_putchar(va_arg(args, int));
 				}
-				else if (format[i] == 's')
+				else if(format[i] == 's')
 				{
+					i++;
 					str = va_arg(args, char *);
-					while (*str)
+					while (str[j] != '\0')
 					{
-						_putchar(*str);
-						str++;
+						_putchar(str[j]);
+						j++;
 					}
 				}
-				else
-					break;
 			}
 			_putchar(format[i]);
 			i++;
 		}
 		va_end(args);
-		return (i + 1);
+		return (i);
 }
