c09fab51b021453fe7cc1d5ccd24316b4d84cfa5
diasbouk <boukdsaid@gmail.com>
2023-10-14 12:26:36 +0100

    task 0

---
+++ main.h
@@ -0,0 +1,9 @@
+#ifndef PRINTF
+#define PRINTF
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+int _putchar(char c);
+int _printf(const char *format, ...);
+#endif
