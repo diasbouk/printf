0def5719534f517cb8e56fc875acd7832b94ec1d
diasbouk <boukdsaid@gmail.com>
2023-10-15 18:31:25 +0100

    task 000000qq

--- _printf.c
+++ _printf.c
@@ -36,7 +36,7 @@ int _printf(const char *format, ...)
 					if (str != NULL)
 						put_string(str);
 					else
-						put_string("(null)");
+						put_string("(null)\n");
 				}
 				else if (format[i] == '%')
 				{
