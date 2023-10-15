2e65529bcb95c92b9090c67c338c8c35e4580032
diasbouk <boukdsaid@gmail.com>
2023-10-15 17:42:53 +0100

    0000

--- main.c
+++ main.c
@@ -7,29 +7,14 @@
  *
  * Return: Always 0
  *
+ */
 int main(void)
 {
-    int len;
-    int len2;
+	int len, len2;
     char * str = "dias .";
 
     printf("%c  %c %%   %s %s   \n", 65, 'a', "string" , str);
    _printf("%c  %c %%   %s %s   \n", 65, 'a', "string", str);
-    len = _printf("Let's try to printf a simple sentence.\n");
-    len2 = printf("Let's try to printf a simple sentence.\n");
-    _printf("Length:[%d, %i]\n", len, len);
-    printf("Length:[%d, %i]\n", len2, len2);
-    _printf("Negative:[%d]\n", -762534);
-    printf("Negative:[%d]\n", -762534);
-    return (0);
-}
-*/
-#include "main.h"
-
-
-int main(void)
-{
-	int len, len2;
 
 	len = _printf("Let's try to printf a simple sentence.\n");
 	len2 = printf("Let's try to printf a simple sentence.\n");
