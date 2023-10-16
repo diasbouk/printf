6eca90b2caa6a6fe5cb11e2a77f4550464c95ef7
yassineedev <yassine.boussif18@gmail.com>
2023-10-16 22:14:41 +0100

    commit 7

--- main.c
+++ main.c
@@ -11,6 +11,7 @@
 int main(void)
 {
 		_printf("Let's try to printf a simple sentence.\n");
+		_printf("this is.\n");
 		printf("Let's try to printf a simple sentence.\n");
 		_printf("test %s\n", "string");
 		_printf("%%\n");
