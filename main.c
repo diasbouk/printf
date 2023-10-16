ee485c93298053c577bb8f648fb7d1991547727b
yassineedev <yassine.boussif18@gmail.com>
2023-10-16 22:25:57 +0100

    commit 21

--- main.c
+++ main.c
@@ -13,6 +13,7 @@ int main(void)
 		_printf("Let's try to printf a simple sentence.\n");
 		_printf("here we go\n");
 		_printf("!\n");
+		_printf("hello world\n");
 		_printf("this is.\n");
 		_printf("dias & enissay\n");
 		_printf("here we go\n");
