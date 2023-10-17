6a62be51b110f8215cf9f786a7efb8ae17e1cd31
yassineedev <yassine.boussif18@gmail.com>
2023-10-17 22:43:43 +0100

    commit n14

--- main.c
+++ main.c
@@ -33,6 +33,7 @@ int main(void)
 		_printf("always 0\n");
 		_printf("char is a character\n");
 		_printf("char\n");
+		_printf("by dias\n");
 		_printf("c is not easy\n");
 		printf("Let's try to printf a simple sentence.\n");
 		_printf("test %s\n", "string");
